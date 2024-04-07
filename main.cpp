0914db9bceafad2a1efe05fd567305b51233a80f
laksh-ag <lakshyaagarwal1@gmail.com>
2024-04-07 11:07:45 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -5,36 +5,34 @@ using namespace std;
 
 int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    int ball_x = 100;
-    int ball_y = 100;
+    const int screenWidth = 900;
+    const int screenHeight = 500;
+    int ballX = 100;
+    int ballY = 100;
     int ball_speed_x = 5;
     int ball_speed_y = 5;
     int ball_radius = 15;
 
-    cout << "Hello World" << endl;
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "My first Pong program!");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false){
         BeginDrawing();
         ClearBackground(BLACK);
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
+        ballX += ball_speed_x;
+        ballY += ball_speed_y;
 
-        if(ball_x + ball_radius >= screenWidth  || ball_x - ball_radius <= 0)
+        if(ballX + ball_radius >= screenWidth  || ballX - ball_radius <= 0)
         {
             ball_speed_x *= -1;
         }
 
-        if(ball_y + ball_radius >= screenHeight  || ball_y - ball_radius <= 0)
+        if(ballY + ball_radius >= screenHeight  || bally - ball_radius <= 0)
         {
             ball_speed_y *= -1;
         }
 
-        DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+        DrawCircle(ballX,ballY,ball_radius, WHITE);
         EndDrawing();
     }
 
